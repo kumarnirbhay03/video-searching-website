f8c261a42ca87818a8f481e1dc5a2317b49ab8d6
kumarnirbhay03 <kumarnirbhayrsvm@gmail.com>
2021-04-02 19:16:38 +0530

    commit 1

--- index.js
+++ index.js
@@ -63,3 +63,187 @@ function signOut() {
   });
   // [END auth_sign_out]
 }
+function addcat() {
+    
+  let addTitle = document.getElementById("addTitle");
+  addTitleval = addTitle.value;
+  
+  if (addTitleval.trim() != 0) {
+      let category = localStorage.getItem("category");
+      
+      if (category == null) {
+          categoryObj = [];
+      } else {
+          categoryObj = JSON.parse(category);
+      }
+      categoryObj.push(addTitle.value);
+      localStorage.setItem("category", JSON.stringify(categoryObj));
+
+      
+      addTitle.value = "";}
+  var n = showCategory();
+  for (let i = 0; i < n; i++){
+      showLink(i);
+  }
+
+}
+
+function showCategory() {
+  
+  let category = localStorage.getItem("category");
+  
+  if (category == null) {
+      categoryObj = [];
+  } else {
+      categoryObj = JSON.parse(category);
+  }
+  let html = "";
+  categoryObj.forEach(function (element, index) {
+      
+      html += `
+  
+  <div class="card-body">
+    <h5 class="card-title">${element}</h5>
+    <button id="${index}" onclick="deleteCat(this.id)" class="btn btn-outline-success">Delete Cat</button>
+    <h5 class="card-title">Added Link</h5>
+    
+      
+    <textarea class="form-control" id="addTxt${index}" rows="1"></textarea>
+    <button id="${index}" onclick="addlink(this.id)"  class="btn btn-outline-success">Add Link</button>
+    <div id="link${index}" class="row container-fluid">
+        
+        
+        
+        
+        </div>
+    
+    
+  </div>
+
+`;
+
+  });
+
+ 
+
+  let categoryElm = document.getElementById("category");
+  if (categoryObj.length !== 0) {
+      categoryElm.innerHTML = html;
+      
+  } else {
+      categoryElm.innerHTML = `Nothing to show! Use "Add a Link" section above to add notes.`;
+     
+  }
+  return categoryObj.length
+}
+
+function addlink(index) {
+ 
+  let addTxt = document.getElementById("addTxt"+index);
+  addTxtval = addTxt.value;
+
+  if (addTxtval.trim() != 0) {
+      let link = localStorage.getItem("link"+index);
+      console.log(link)
+      if (link == null) {
+          linkObj = [];
+      } else {
+          linkObj = JSON.parse(link);
+      }
+
+      
+      linkObj.push(addTxt.value);
+      
+      localStorage.setItem("link"+index, JSON.stringify(linkObj));
+
+      addTxt.value = "";
+  }
+
+  showLink(index);
+}
+
+function showLink(index) {
+  
+  let link = localStorage.getItem("link"+index);
+  
+  if (link == null) {
+      linkObj = [];
+  } else {
+      linkObj = JSON.parse(link);
+  }
+  
+  let htmlc = "";
+  linkObj.forEach(function (item, i) {
+      htmlc += `
+  <a href="${item}" class="card-text"> ${item}</a>
+  <button id="${i}" onclick="deleteNote(${index}, this.id)" class="btn btn-outline-success">Delete Link</button>
+  
+`;
+
+  });
+
+
+ 
+
+  let linkElm = document.getElementById("link"+index);
+  if (linkObj.length !== 0) {
+      linkElm.innerHTML = htmlc;
+  } else {
+      linkElm.innerHTML = `Nothing to show! Use "Add Link ".`;
+  }
+  return linkObj.length 
+}
+
+function deleteNote(index, i) {
+
+
+  let link = localStorage.getItem("link"+index);
+  if (link == null) {
+      linkObj = [];
+  } else {
+      linkObj = JSON.parse(link);
+  }
+
+  linkObj.splice(i, 1);
+  localStorage.setItem("link"+index, JSON.stringify(linkObj));
+  showLink(index);
+}
+
+function deleteCat(index) {
+
+
+  let category = localStorage.getItem("category");
+  if (category == null) {
+      categoryObj = [];
+  } else {
+      categoryObj = JSON.parse(category);
+  }
+
+  categoryObj.splice(index, 1);
+  localStorage.setItem("category", JSON.stringify(categoryObj));
+  deleteAll(index);
+  let n = showCategory();
+  for (let i = 0; i < n; i++){
+      showLink(i);
+  }
+}
+
+
+
+function deleteAll(index){
+console.log(this,index);
+let link = localStorage.getItem("link"+index);
+  if (link == null) {
+      linkObj = [];
+  } else {
+      linkObj = JSON.parse(link);
+  }
+  let n=showLink(index);
+  linkObj.splice(0,n);
+  
+  localStorage.setItem("link"+index, JSON.stringify(linkObj));
+  
+  
+  showLink(index);
+
+}
